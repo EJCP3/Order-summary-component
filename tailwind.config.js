0441b6dc55d3c95e65d705d963543ee9a4ffc870
EJCP3 <euddycabrera03@gmail.com>
2024-05-04 22:04:21 -0400

    v1-html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+
+      fontFamily: {
+        "principal": ['Red Hat Display, sans-serif'],
+      },
+
+
+      colors:{
+        //Primary
+        "Pale-blue": "hsl(225, 100%, 94%)",
+         "Bright-blue": "hsl(245, 75%, 52%)",
+        //Neutral
+         "Very-pale-blue": "hsl(225, 100%, 98%)",
+         "Desaturated-blue": "hsl(224, 23%, 55%)",
+         "Dark-blue": "hsl(223, 47%, 23%)",
+      },
+
+     
+
+    },
   },
   plugins: [],
 }
